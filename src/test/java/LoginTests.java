b0d449fc7a52b08dee970e94a7c3983d7f8d57a7
Quthonn <gevorgyan.genri@gmail.com>
2025-03-07 00:05:51 +0700

    Commit

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,16 +1,27 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverConditions.url;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginTests { //Добавить везде этапы проверок
+
+    private static String MAIL;
+    private static String PASSWORD;
+
     @BeforeAll
-    public static void BeforeAll() { //Для обхода проблемы со входом в Gmail при переходе на страницу ввода пароля
+    public static void BeforeAll() {
+        Dotenv dotenv = Dotenv.load();
+        MAIL = dotenv.get("MAIL");
+        PASSWORD = dotenv.get("PASSWORD");
+
+        //Для обхода проблемы со входом в Gmail при переходе на страницу ввода пароля
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--disable-blink-features=AutomationControlled");
@@ -21,12 +32,14 @@ public class LoginTests { //Добавить везде этапы провер�
         Configuration.holdBrowserOpen = true;
     }
 
+
+
     @Test
     public void successfulLogin_Gmail() {
         open("https://gmail.com");
-        $("[name=identifier]").setValue("MAIL");
+        $("[name=identifier]").setValue(MAIL);
         $(".TNTaPb").click(); //Нажатие по кнопке перехода на страницу ввода пароля
-        $("[name=Passwd]").shouldBe(visible).setValue("PASSWORD");
+        $("[name=Passwd]").shouldBe(visible).setValue(PASSWORD);
         $(".TNTaPb").click();
 
         // Ожидание загрузки страницы
